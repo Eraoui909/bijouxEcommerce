1c302ffffb9fec2850e62f2345f65be8ba901196
Hamza Eraoui <hamzaprofessionnel1234@gmail.com>
2022-01-20 00:29:36 +0100

    upload and delete pics

--- resources/views/backOffice/layout/includes/navbar.blade.php
+++ resources/views/backOffice/layout/includes/navbar.blade.php
@@ -77,7 +77,7 @@
                         <i class="ti-settings text-primary"></i>
                         Settings
                     </a>
-                    <a class="dropdown-item">
+                    <a class="dropdown-item" href="{{ route("admin.logout") }}">
                         <i class="ti-power-off text-primary"></i>
                         Logout
                     </a>
